bc9a6e36a166aec2db82ac7f2bfb1aaf3189f686
Prayz30 <praisewithfaith@gmail.com>
2022-03-16 07:48:18 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main block
+ * description - write a program that prints _putchar, followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *pb = "_putchar";
+
+	while (*pb)
+	{
+		_putchar(*pb);
+		pb++;
+	}
+	_putchar('\n');
+
+return (0);
+}

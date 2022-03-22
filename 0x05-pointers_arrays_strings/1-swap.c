e2202c60547c8973c780c2c1942902f824064bb6
Prayz30 <praisewithfaith@gmail.com>
2022-03-22 05:48:59 -0700

    swapping digits

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int -  a function that swaps the values of two integers.
+ * @a: an inout integer pointer
+ * @b: an inout integer pointer
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int change;
+
+	change = *a;
+	*a = *d;
+	*d = change;
+}

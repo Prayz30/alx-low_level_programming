babd57af15ea33f2aa6958c663c626a3eabe7c1c
Prayz30 <praisewithfaith@gmail.com>
2022-03-22 05:22:21 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * description - point to 98
+ * @n: - pinter to int
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}

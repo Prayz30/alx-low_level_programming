3061334e965fceef2718a0e10314b5643b3354bf
Prayz30 <praisewithfaith@gmail.com>
2022-03-24 12:59:06 -0700

    compare string and reverse array

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1 : pointer to char params
+ * @s2 : pointer to char params
+ * Return: The difference s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}

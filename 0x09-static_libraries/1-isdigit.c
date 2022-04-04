7a6c4ca6f95565599d0483abe926e1076a7bfa70
Prayz30 <praisewithfaith@gmail.com>
2022-04-04 13:52:17 -0700

    malloc

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isdigit - check if c is a digit between 0 and 9.
- * @c: the integer to be supplied by user.
+ * @c: the inhnteger to be supplied by user.
  * Return: 0 or 1.
  */
 int _isdigit(int c)
